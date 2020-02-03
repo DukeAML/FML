98570fe68f906861bc534a7311f0f9b27ac7c4db
jackmoody11 <jackmoody@unc.edu>
2020-02-02 23:17:10 -0500

    Flake8

--- algDev/test.py
+++ algDev/test.py
@@ -1,4 +1,4 @@
-from models.equity import equity 
+from models.equity import equity
 from models.indicators import *
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -10,7 +10,7 @@ vslr = equity(vslr_file)
 a = true_range(vslr, 10)
 b = true_range(vslr, 20)
 
-print (b[45])
+print(b[45])
 plt.plot(a[40:], 'r-')
 plt.plot(b[40:], 'b-')
 plt.show()
