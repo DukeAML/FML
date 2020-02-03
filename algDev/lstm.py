3bb0fd57e6bde7eaea6270d396f4c9fd447dd1be
jackmoody11 <jackmoody@unc.edu>
2020-02-02 23:35:34 -0500

    Fix rebase issues

--- algDev/lstm.py
+++ algDev/lstm.py
@@ -1,24 +1,22 @@
-# from __future__ import absolute_import, division, print_function, unicode_literals
 import tensorflow as tf
-<<<<<<< HEAD
-from tensorflow import keras 
+from tensorflow import keras
 from keras.models import Sequential
 from keras.layers import BatchNormalization, LeakyReLU, LSTM, Dense, Dropout
 from keras.utils import plot_model
 from gen_lstm_data import gen_data, gen_labels, get_data_labelled
 
+# input should be [batch_size, time_steps, features] = [30, 19, 25]
+# labels = 10 classes
 
-#input should be [batch_size, time_steps, features] = [30, 19, 25]
-#labels = 10 classes
-
-(X_train, y_train, X_test, y_test) = gen_data(eq = "VSLR", verbose= True)
+(X_train, y_train, X_test, y_test) = gen_data(eq="VSLR", verbose=True)
 print(y_train[0])
 print(X_train[0])
 
-#first model (w/o text pipeline)
+
+# first model (w/o text pipeline)
 def create_model1a():
     model = Sequential()
-    model.add(LSTM(125,  dropout = .2, input_shape= (19,25) ))
+    model.add(LSTM(125, dropout=.2, input_shape=(19, 25)))
     model.add(BatchNormalization())
     model.add(Dense(64))
     model.add(LeakyReLU())
@@ -26,33 +24,35 @@ def create_model1a():
     model.add(Dense(28))
     model.add(LeakyReLU())
     model.add(BatchNormalization())
-    model.add(Dense(10, activation = "softmax"))
+    model.add(Dense(10, activation="softmax"))
     return model
+
 
 model1a = create_model1a()
 
+
 def create_model1b():
     model = Sequential()
-    model.add(LSTM(125,  dropout = .2, return_sequences=True, input_shape= (19,25)))
-    model.add(LSTM(125,  dropout = .2, return_sequences=True))
-    model.add(LSTM(125,  dropout = .2, return_sequences=False))
+    model.add(LSTM(125, dropout=.2, return_sequences=True, input_shape=(19, 25)))
+    model.add(LSTM(125, dropout=.2, return_sequences=True))
+    model.add(LSTM(125, dropout=.2, return_sequences=False))
     model.add(BatchNormalization())
     model.add(Dense(64))
     model.add(LeakyReLU())
     model.add(Dropout(.2))
     model.add(BatchNormalization())
-    model.add(Dense(10, activation = "softmax"))
+    model.add(Dense(10, activation="softmax"))
     return model
+
 
 model1b = create_model1b()
 
 # plot_model(model1b, to_file='model1b.png')
 
 model1a.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['categorical_accuracy', 'mae', 'mse'])
-model1a.fit(X_train, y_train, batch_size=30, epochs=500, verbose= 2, validation_split= .2)
+model1a.fit(X_train, y_train, batch_size=30, epochs=500, verbose=2, validation_split=.2)
 score = model1a.evaluate(X_test, y_test, batch_size=30, verbose=2)
 print(score)
-
 
 # model1b.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['categorical_accuracy', 'mae', 'mse'])
 # model1b.fit(X_train, y_train, batch_size=30, epochs=500, verbose= 2, validation_split= .2)
@@ -60,11 +60,7 @@ print(score)
 # print(score)
 
 # we want labels to look like [batch, 19, 1]
-=======
-from tensorflow import keras
-from tensorflow.keras import layers, models
-from keras.utils.vis_utils import model_to_dot
-from gensim.models import KeyedVectors
+
 
 # from gensim.models.word2vec import Word2Vec
 # import gensim.downloader as api
@@ -107,4 +103,3 @@ model = tf.keras.Model([model_inputNum, model_inputText], model_output)
 model.summary()
 
 keras.utils.plot_model(model, 'my_first_model.png')
->>>>>>> f5da21470380437f7e57d43fd04369164daca4c2
