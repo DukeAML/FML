e3c6f939e5b84da9e2ba2176585fccffd836bd31
Phoebe Klett <plk6@duke.edu>
2020-02-19 16:14:51 -0500

    cont. work to get time diff for derivatives paid

--- algDev/div.py
+++ algDev/div.py
@@ -15,10 +15,34 @@ for tckr in tickers:
 
 div = div.fillna(0)
 
-paid=[]
-for ii in range(len(div["aapl"])):
-    if div["aapl"][ii] != 0: 
-        paid.append(ii)
+for tckr in tickers:
+    paid=[]
+    for ii in range(len(div[tckr])):
+        if div[tckr][ii] != 0: 
+            paid.append(ii)
+    if not paid:
+        print(tckr + " has issued no derivatives")   
+    else:
+
+        count = [999 for ii in range(len(div[tckr]))]
+
+        for ii in range(len(count)):
+            if ii in paid: 
+                count[ii] = 0
+            else:
+                if ii < min(paid):
+                    count[ii] = "na"
+                else:
+                    c =0
+                    tmp = ii
+                    while ii not in paid and ii > min(paid):
+                        ii -= 1 
+                        c+=1
+                    count[tmp] = c
+            div["Passing Time: " + tckr] = count
+
+
+print(div.head())
 
 
 
