6344dc1e509af176ffda26cce0a500387e14ca4b
Jaiveer Katariya <jk386@duke.edu>
2020-02-26 22:24:22 -0500

    added credentials structure

--- backend/db/primer.py
+++ backend/db/primer.py
@@ -1,12 +1,12 @@
 import psycopg2
 from openpyxl import load_workbook
+import credentials
 
 from os import listdir
 from os.path import isfile, join
 
-conn = psycopg2.connect(host="localhost",database="postgres", user="postgres", password="daddyluke123")
+conn = psycopg2.connect(host="localhost",database="postgres", user=credentials.username, password=credentials.password)
 conn.autocommit = True
-
 cursor = conn.cursor()
 
 # Print PostgreSQL Connection properties
