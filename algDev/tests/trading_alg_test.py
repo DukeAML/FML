447e832b09156a3931a2f9f94305f1a4054a4231
Phoebe Klett <plk6@duke.edu>
2020-04-11 21:30:29 -0400

    fucked around - no real changes

--- algDev/tests/trading_alg_test.py
+++ algDev/tests/trading_alg_test.py
@@ -10,12 +10,12 @@ def run_test_one():
     ta.plot_models_rocs()
 
 def build_confusion_matrix():
-    tickers = ['AAPL']
+    tickers = ['NFLX']
     features = ['macd_9_18','closes']
-    ta = TradingAlgorithm(tickers, features, type = 'svm', data_lookback_period = 10, label_lower_threshold = -0.15, label_upper_threshold = 0.015, label_period = 10, data_splits = [0.8, 0.2], cnn_split=0, verbose=True)
+    ta = TradingAlgorithm(tickers, features, type = 'svm', data_lookback_period = 15, label_lower_threshold = -0.15, label_upper_threshold = 0.015, label_period = 10, data_splits = [0.8, 0.2], cnn_split=0, verbose=True)
     print(ta.models[0].accuracy)
     ## At this point, the model is trained for everything. 
-    ta.generate_conf_matricies(datetime.date(2018,1,1), datetime.date(2020,3,15),True)
+    ta.generate_conf_matricies(datetime.date(2018,1,1), datetime.date(2020,3,15))
 
 def test_conf_matrix_model_coll():
     tickers = ['AAPL']
