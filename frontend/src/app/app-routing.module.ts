84a75355908fa6dc50ba4cde4c777e195257198a
Jaiveer Katariya <jk386@duke.edu>
2020-04-14 01:30:09 -0400

    made params modal, now to visualize test results

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { BotComponent } from './components/bot/bot.component';
 import { AboutComponent } from './components/about/about.component';
+import { BacktesterComponent } from './components/backtester/backtester.component';
 
 
 const routes:Routes = [
   { path: 'dashboard', component: DashboardComponent, pathMatch: 'full' },
   { path: 'bot', component: BotComponent, pathMatch: 'full'},
+  { path: 'backtester', component: BacktesterComponent, pathMatch: 'full'},
   { path: 'about', component: AboutComponent, pathMatch: 'full'},
   { path: '', redirectTo:'/bot', pathMatch: 'full'}
 ];
