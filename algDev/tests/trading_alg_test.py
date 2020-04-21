87cf622384a0a16c762ae3a9dfa120d61b34bbc9
Phoebe Klett <plk6@duke.edu>
2020-04-21 15:43:54 -0400

    testing

--- algDev/tests/trading_alg_test.py
+++ algDev/tests/trading_alg_test.py
@@ -7,10 +7,10 @@ import csv
 def run_test_one():
     tickers = ['AAPL']
     features = ['macd_9_18','closes','prings','kst','kstTrix', 'rsi']
-    ta = TradingAlgorithm(tickers, features, type = 'svm', data_lookback_period = 10, label_lower_threshold = -0.15, label_upper_threshold = 0.005, label_period = 1, data_splits = [0.8, 0.2], cnn_split=0, model_params ={'gamma':10,"C":100}, verbose=True)
+    ta = TradingAlgorithm(tickers, features, type = 'svm', data_lookback_period = 15, label_lower_threshold = -0.05, label_upper_threshold = 0.005, label_period = 10, data_splits = [0.8, 0.2], cnn_split=0, model_params ={'gamma':10,"C":100}, verbose=True)
     print(ta.models[0].accuracy)
     # ta.generate_conf_matricies(datetime.date(2018,1,1), datetime.date(2019,1,1))
-    ta.plot_models_rocs()
+    # ta.plot_models_rocs()
 
 def build_confusion_matrix():
     # one cm for whole alg 
