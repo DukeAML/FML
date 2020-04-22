e378a036ee23bb7a18c0b181cd998b6850bdbc28
LUKE TRUITT <lot@duke.edu>
2020-04-22 13:55:58 -0500

    positions

--- algDev/models/backtest.py
+++ algDev/models/backtest.py
@@ -66,7 +66,7 @@ class Backtest():
         dates, pf_vals, initial_val, snp_vals = self.get_pf_values()
         stats = [{'name': 'return', 'value':rtn}, {'name':'snp_return', 'value':snp_rtn}, {'name':'net_return', 'value':net_rtn}, {'name':'average_free_cash', 'value':avg_free_cash}, {'name':'beta', 'value': beta}, {'name':'vol','value':vol}, {'name':'treynor', 'value':treynor}, {'name':'sharpe','value':sharpe}]
         positions = self.gen_positions(self.portfolio.positions)
-        return_val = {'stats': stats, 'dates':dates, 'portfolioValues':pf_vals, 'initialValues':initial_val, 'snpVals':snp_vals, 'positions':self.portfolio.positions}
+        return_val = {'stats': stats, 'dates':dates, 'portfolioValues':pf_vals, 'initialValues':initial_val, 'snpVals':snp_vals, 'positions':positions}
 
         return return_val
     def get_pf_values(self):
