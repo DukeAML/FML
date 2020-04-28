44f0837342e070c099256019891e1937907866f1
Phoebe Klett <plk6@duke.edu>
2020-04-28 00:05:42 -0400

    testing

--- algDev/tests/trading_alg_test.py
+++ algDev/tests/trading_alg_test.py
@@ -7,8 +7,8 @@ import csv
 
 def run_test_one():
     tickers = ['AAPL']
-    features = ['macd_9_18','closes','kst','rsi']
-    ta = TradingAlgorithm(tickers, features, type = 'svm', data_lookback_period = 10, label_lower_threshold = -0.15, label_upper_threshold = 0.035, label_period = 10, voting_type = 'accuracy', model_params ={'gamma':100,"C":1}, data_splits = [0.8, 0.2], cnn_split=0, verbose=True, test_mode = True)
+    features = ['macd_9_18','closes','prings','kst','kstTrix', 'rsi']
+    ta = TradingAlgorithm(tickers, features, type = 'svm', data_lookback_period = 10, label_lower_threshold = -0.15, label_upper_threshold = 0.035, label_period = 10, voting_type = 'Penrose', model_params ={'gamma':100,"C":1}, data_splits = [0.8, 0.2], cnn_split=0, verbose=True, test_mode = True)
     print(ta.models[0].accuracy)
     ta.generate_conf_matricies(datetime.datetime(2018,1,1), datetime.datetime(2019,1,1))
     # ta.plot_models_rocs()
@@ -17,7 +17,7 @@ def build_confusion_matrix():
     # one cm for whole alg 
     tickers = ['AAPL']
     features = ['macd_9_18','closes','prings','kst','kstTrix', 'rsi']
-    ta = TradingAlgorithm(tickers, features, type = 'svm', data_lookback_period = 15, label_lower_threshold = -0.15, label_upper_threshold = 0.015, label_period = 10, data_splits = [0.8, 0.2], model_params ={'gamma':100,"C":100}, cnn_split=0, verbose=True)
+    ta = TradingAlgorithm(tickers, features, type = 'svm', data_lookback_period = 10, label_lower_threshold = -0.15, label_upper_threshold = 0.035, label_period = 10, voting_type = 'accuracy', model_params ={'gamma':100,"C":1}, data_splits = [0.8, 0.2], cnn_split=0, verbose=True, test_mode = True)
     print(ta.models[0].accuracy)
     ## At this point, the model is trained for everything. 
     ta.generate_conf_matricies(datetime.datetime(2018,1,1), datetime.datetime(2020,3,15))
